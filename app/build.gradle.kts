55627355ce4d6f3fae4059877e643397d4ccbd27
xibuka <shibunkan@gmail.com>
2025-12-12 22:35:01 +0900

    update version for bouncycastle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     testImplementation("junit:junit:4.13.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    classpath("org.bouncycastle:bcutil-jdk18on:1.79")
 }
 
 sonar {
